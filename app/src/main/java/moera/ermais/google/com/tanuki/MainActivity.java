e4adeea47ab92dbad3882e08754007f7d786868b
Chubanova <m.esaeva@inbox.ru>
2018-12-22 14:49:32 +0300

    add valid

--- app/src/main/java/moera/ermais/google/com/tanuki/MainActivity.java
+++ app/src/main/java/moera/ermais/google/com/tanuki/MainActivity.java
@@ -1,12 +1,18 @@
 package moera.ermais.google.com.tanuki;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.regex.Pattern;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -38,16 +44,71 @@ public class MainActivity extends AppCompatActivity {
     Button mSave;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        String [] payTypes={getString(R.string.payment_card_restaurant),getString(R.string.payment_encash)};
+        String[] payTypes = {getString(R.string.payment_card_restaurant), getString(R.string.payment_encash)};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, payTypes);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mPayTypeS.setAdapter(adapter);
+
+        isValidPhoneOrEmail(mEmailET.getText(), Patterns.EMAIL_ADDRESS);
+        isValidPhoneOrEmail(mPhoneET.getText(), Patterns.PHONE);
+//        boolean = mCountPersonET.getText()
+        mSave.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                if (validForm()){
+
+                }
+                else{
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+
+                    builder.setMessage(R.string.dialog_message)
+                            .setTitle(R.string.dialog_title);
+
+                    AlertDialog dialog = builder.create();
+                }
+
+
+            }
+        });
+
     }
+
+    public final static boolean isValidPhoneOrEmail(CharSequence target, Pattern pattern) {
+        return !TextUtils.isEmpty(target) && pattern.matcher(target).matches();
+    }
+
+    boolean validForm() {
+        boolean formIsValid = true;
+        if (isValidPhoneOrEmail(mEmailET.getText(), Patterns.EMAIL_ADDRESS)) {
+            mEmailET.setError("Enter valid email");
+            formIsValid = false;
+        }
+        if (isValidPhoneOrEmail(mPhoneET.getText(), Patterns.PHONE)) {
+            mPhoneET.setError("Enter valid phone");
+            formIsValid = false;
+
+        }
+        int countPerson = Integer.parseInt(mCountPersonET.getText().toString());
+        if (countPerson > 10) {
+            mCountPersonET.setError("Персон должно быть меньшне 10");
+            formIsValid = false;
+
+        }
+        int countItem = Integer.parseInt(mCountItemsET.getText().toString());
+        if (countItem <= 0) {
+            mCountItemsET.setError("Количество товаров должно быть больше 0");
+            formIsValid = false;
+
+        }
+
+        return formIsValid;
+    }
+
 }
